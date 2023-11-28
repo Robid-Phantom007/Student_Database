99e221ce1c145754cc071c889b7b0f2c853c3504
Robid-Phantom007 <robidnomolos@gmail.com>
2023-11-28 23:00:27 +0100

    Updated the update and delete function

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -42,3 +42,103 @@ exports.getAllStudent = async (req, res)=>{
     })
   }
 }
+
+// get a student
+
+exports.getStudent = async(req,res)=>{
+  try{
+    const studentId = req.params.ID;
+  
+    const student = await studentModel.findById(studentId);
+ if(!student){
+    res.status(404).json({
+        message:"student not found"
+    })
+ }else{
+    res.status(200).json({
+        message:"student found",
+        data:student
+    })
+ }
+  }catch(error){
+    res.status(404).json({
+        message:error
+    })
+} 
+}
+
+//update a student
+exports.updateStudent = async(req,res)=>{
+    try{
+    //track the user id
+    const studentId = req.params.ID;
+
+    //track the student with the id gotten
+  const student = await studentModel.findById(studentId);
+
+      //const updatedScore = req.body.score ?? student.score;
+      const updatedScore = {
+        html: req.body.score?.html !== undefined && req.body.score?.html !== '' ? req.body.score.html : 0,
+        javascript: req.body.score?.javascript !== undefined ? req.body.score.javascript : 0,
+        css: req.body.score?.css !== undefined && req.body.score?.css !== '' ? req.body.score.css : 0,
+        node: req.body.score?.css !== undefined && req.body.score?.css !== '' ? req.body.score.css : 0,
+    };
+
+    //check for entity and replace with existing data
+    const studentData ={
+            name:req.body.name ?? student.name,
+            stack:req.body.stack ?? student.stack,
+            score:updatedScore
+        };
+        
+    //update the student
+    const updatedStudent = await studentModel.findByIdAndUpdate(
+            studentId, 
+            studentData,
+        
+        {
+                new: true,
+            });
+      //checkfor errors
+        if (!updatedStudent){
+            res.status(404).json({
+                message: `Student with id: ${studentId} is not found`
+            });
+        }else{
+            res.status(200).json({
+                message: `Student with id: ${studentId} has been updated`,
+                data:updatedStudent,
+            });
+        }   
+
+    }catch(err){
+        res.status(500).json({
+            message: err.message});
+    } 
+}
+
+   //delete the student
+exports.deleteStudent = async(req,res)=>{
+    try{
+     //track the user id
+        const studentId = req.params.ID;
+
+    //track the student with the ID gotten
+    const student = await studentModel.findById(studentId);
+//check for errors
+if(!student){
+    res.staus(404).json({
+        message: `student with id ${studentId} has not been found`
+    })
+}
+//delete the student
+await studentModel.findByIdAndDelete(student);
+return res.status(200).json({
+    message:`student with id ${studentId} has been deleted`,
+    data: student,
+})
+
+    }catch(error){
+        console.log(error);
+    }
+  }
--- routers/studentRouter.js
+++ routers/studentRouter.js
@@ -1,9 +1,15 @@
 const express = require('express')
-const {createStudent, getAllStudent} = require('../controllers/studentController')
+const {createStudent, getAllStudent, getStudent, updateStudent, deleteStudent} = require('../controllers/studentController')
 const router = express.Router()
 
 router.post('/create', createStudent)
 
 router.get('/getall',getAllStudent)
+
+router.get('/getone/:ID', getStudent)
+
+router.put('/update/:ID', updateStudent)
+
+router.delete('/delete/:ID', deleteStudent)
 
 module.exports = router
